4f054d5200f34c55241ff6403b5a9a49f6979c6d
Inzamum-Ul-Haque <inzamum.inu@gmail.com>
2023-03-22 15:47:52 +0600

    commit

--- src/pages/Home/Latest/Latest.js
+++ src/pages/Home/Latest/Latest.js
@@ -1,35 +1,9 @@
 import React from "react";
 import "./Latest.css";
-import LatestImage1 from "../../../assets/images/unsplash-1.jpg";
-import LatestImage2 from "../../../assets/images/unsplash-2.jpg";
-import LatestImage3 from "../../../assets/images/unsplash-3.jpg";
 import LatestImageSection from "../../../components/LatestImageSection/LatestImageSection";
+import { latestBlogs } from "../../../staticData/LatestBlogs";
 
 const Latest = () => {
-  const images = [
-    {
-      image: LatestImage1,
-      name: "Naveen Chari",
-      date: "Feb 20, 2020",
-      title: "Help out the people who really need it on time.",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum quos doloremque, harum atque voluptatum labore.",
-    },
-    {
-      image: LatestImage2,
-      name: "Naveen Chari",
-      date: "Feb 20, 2020",
-      title: "Help out the people who really need it on time.",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum quos doloremque, harum atque voluptatum labore.",
-    },
-    {
-      image: LatestImage3,
-      name: "Naveen Chari",
-      date: "Feb 20, 2020",
-      title: "Help out the people who really need it on time.",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum quos doloremque, harum atque voluptatum labore.",
-    },
-  ];
-
   return (
     <div id="latest" className="latest-container">
       <div className="latest-wrapper">
@@ -41,7 +15,7 @@ const Latest = () => {
           </p>
         </div>
         <div className="latest-img-container">
-          {images.map((image, idx) => (
+          {latestBlogs.map((image, idx) => (
             <LatestImageSection key={idx} image={image} />
           ))}
         </div>
---
+++ src/staticData/LatestBlogs.js
@@ -0,0 +1,27 @@
+import LatestImage1 from "../assets/images/unsplash-1.jpg";
+import LatestImage2 from "../assets/images/unsplash-2.jpg";
+import LatestImage3 from "../assets/images/unsplash-3.jpg";
+
+export const latestBlogs = [
+  {
+    image: LatestImage1,
+    name: "Naveen Chari",
+    date: "Feb 20, 2020",
+    title: "Help out the people who really need it on time.",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum quos doloremque, harum atque voluptatum labore.",
+  },
+  {
+    image: LatestImage2,
+    name: "Naveen Chari",
+    date: "Feb 20, 2020",
+    title: "Help out the people who really need it on time.",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum quos doloremque, harum atque voluptatum labore.",
+  },
+  {
+    image: LatestImage3,
+    name: "Naveen Chari",
+    date: "Feb 20, 2020",
+    title: "Help out the people who really need it on time.",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum quos doloremque, harum atque voluptatum labore.",
+  },
+];
