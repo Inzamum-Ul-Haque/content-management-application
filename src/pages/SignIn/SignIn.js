a0889de5c09237650541f205f927463300ad18a4
Inzamum-Ul-Haque <inzamum.inu@gmail.com>
2023-03-23 19:06:24 +0600

    designed sign-up page

--- src/pages/SignIn/SignIn.js
+++ src/pages/SignIn/SignIn.js
@@ -5,23 +5,36 @@ import "./SignIn.css";
 const SignIn = () => {
   return (
     <div className="sign-in-container">
-      <div className="form-wrap">
+      <div className="signin-form-wrap">
         <Link to="/" className="sign-in-logo">
           somewhereinblog.com
         </Link>
-        <div className="form-content">
+        <div className="signin-form-content">
           <form className="signin-form">
             <h1>Sign In to your account</h1>
             <label className="form-label" htmlFor="for">
               Email
             </label>
-            <input className="form-input" type="email" required />
+            <input
+              className="form-input"
+              type="email"
+              required
+              placeholder="E-mail"
+            />
             <label className="form-label" htmlFor="for">
               Password
             </label>
-            <input className="form-input" type="password" required />
+            <input
+              className="form-input"
+              type="password"
+              required
+              placeholder="Password"
+            />
+            <span className="forgot-pass-text">Forgot Password?</span>
             <button>Continue</button>
-            <span>Forgot Password?</span>
+            <span className="continue-to-sign-up-text">
+              Don't have an account? <Link to="/sign-up">Sign Up</Link> Now
+            </span>
           </form>
         </div>
       </div>
--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -1,7 +1,63 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./SignUp.css";
 
 const SignUp = () => {
-  return <div>sign up</div>;
+  return (
+    <div className="signup-container">
+      <div className="signup-form-wrap">
+        <Link to="/" className="sign-up-logo">
+          somewhereinblog.com
+        </Link>
+        <div className="signup-form-content">
+          <form className="signup-form">
+            <h1>Create an account</h1>
+            <label className="form-label" htmlFor="for">
+              Username
+            </label>
+            <input
+              className="form-input"
+              type="text"
+              required
+              placeholder="Type a username"
+            />
+            <label className="form-label" htmlFor="for">
+              Email
+            </label>
+            <input
+              className="form-input"
+              type="email"
+              required
+              placeholder="Type your e-mail"
+            />
+            <label className="form-label" htmlFor="for">
+              Password
+            </label>
+            <input
+              className="form-input"
+              type="password"
+              required
+              placeholder="Enter a password"
+            />
+            <label className="form-label" htmlFor="for">
+              Confirm Password
+            </label>
+            <input
+              className="form-input"
+              type="password"
+              required
+              placeholder="Type your password again"
+            />
+
+            <button>Sign Up</button>
+            <span className="continue-to-sign-in-text">
+              Already have an account? <Link to="/sign-in">Sign In</Link> Now
+            </span>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
