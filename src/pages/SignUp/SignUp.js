51f9e20f18a0b2cfdbc3ba862f7731732121a919
Inzamum-Ul-Haque <inzamum.inu@gmail.com>
2023-03-29 15:25:37 +0600

    commit

--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -1,18 +1,34 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../contexts/AuthProvider";
+import { errorMessage } from "../../staticData/authErrors";
 import "./SignUp.css";
 
 const SignUp = () => {
-  const [error, setError] = useState("this is error");
+  const [passwordMatchError, setPasswordMatchError] = useState("");
+  const [signupError, setSignupError] = useState("");
+  const { signInUser } = useContext(AuthContext);
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
 
-  const handleSignUp = (e) => {
-    e.preventDefault();
+  const handleSignUp = (data) => {
+    setPasswordMatchError("");
+    setSignupError("");
+
+    if (data.password !== data.confirmPassword) {
+      setPasswordMatchError("Password do not match");
+      return;
+    }
+
+    signInUser(data.email, data.password)
+      .then((result) => {})
+      .catch((error) => {
+        setSignupError(errorMessage(error.message));
+      });
   };
 
   return (
@@ -22,50 +38,79 @@ const SignUp = () => {
           somewhereinblog.com
         </Link>
         <div className="signup-form-content">
-          <form onSubmit={handleSignUp} className="signup-form">
+          <form onSubmit={handleSubmit(handleSignUp)} className="signup-form">
             <h1>Create an account</h1>
-            <label className="form-label" htmlFor="for">
-              Username
-            </label>
-            <input
-              className="form-input"
-              name="username"
-              type="text"
-              required
-              placeholder="Type a username"
-            />
-            <label className="form-label" htmlFor="for">
-              Email
-            </label>
-            <input
-              className="form-input"
-              name="email"
-              type="email"
-              required
-              placeholder="Type your e-mail"
-            />
-            <label className="form-label" htmlFor="for">
-              Password
-            </label>
-            <input
-              className="form-input"
-              name="password"
-              type="password"
-              required
-              placeholder="Enter a password"
-            />
-            <label className="form-label" htmlFor="for">
-              Confirm Password
-            </label>
-            <input
-              className="form-input confirm-pass"
-              name="confirmPassword"
-              type="password"
-              required
-              placeholder="Type your password again"
-            />
-            {error && <span className="error-text">{error}</span>}
+            <div className="input-container">
+              <label className="form-label" htmlFor="for">
+                Username
+              </label>
+              <input
+                className="form-input"
+                {...register("username", {
+                  required: "Please give a username",
+                })}
+                type="text"
+                placeholder="Type a username"
+              />
+              {errors.username && (
+                <span className="error-text">{errors.username.message}</span>
+              )}
+            </div>
+            <div className="input-container">
+              <label className="form-label" htmlFor="for">
+                Email
+              </label>
+              <input
+                className="form-input"
+                {...register("email", {
+                  required: "Please enter your e-mail",
+                })}
+                type="email"
+                placeholder="Type your e-mail"
+              />
+              {errors.email && (
+                <span className="error-text">{errors.email.message}</span>
+              )}
+            </div>
+            <div className="input-container">
+              <label className="form-label" htmlFor="for">
+                Password
+              </label>
+              <input
+                className="form-input"
+                {...register("password", {
+                  required: "Please provide password",
+                })}
+                type="password"
+                placeholder="Enter a password"
+              />
+              {errors.password && (
+                <span className="error-text">{errors.password.message}</span>
+              )}
+            </div>
+            <div className="input-container">
+              <label className="form-label" htmlFor="for">
+                Confirm Password
+              </label>
+              <input
+                className="form-input"
+                {...register("confirmPassword", {
+                  required: "Please confirm password",
+                })}
+                type="password"
+                placeholder="Type your password again"
+              />
+              {errors.confirmPassword && (
+                <span className="error-text">
+                  {errors.confirmPassword.message}
+                </span>
+              )}
+              {passwordMatchError && (
+                <span className="error-text">{passwordMatchError}</span>
+              )}
+            </div>
             <button type="submit">Sign Up</button>
+            {signupError && <span className="error-text">{signupError}</span>}
             <span className="continue-to-sign-in-text">
               Already have an account? <Link to="/sign-in">Sign In</Link> Now
             </span>
